0f3d2e8bcfc21545a4ed9ab8ef889f00e7006afc
Tiaan <tiaanduplessis@hotmail.com>
2016-11-10 14:52:33 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
+![pomd demo](media/demo.gif)
+
 # pomd
-🍅 Just a good old cli based pomodoro timer with  native notifactions
+[![Standard - JavaScript Style Guide](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
+
+🍅 Just a good old cli based pomodoro timer with  native notifactions.
+
+## Install
+
+```
+$ npm install -g pomd
+```
+
+## Usage
+
+1. Run `pomd` in your terminal.
+2. Run `help` to see the options available.
+
+## TODO
+
+- Add ability to run continues sessions.
+
+## Thanks to OSS
+
+pomd is made possible through Open Source Software. A very special thanks to all the modules pomd uses [uses](package.json).
+
+## Contributing
+All Contributions are welcome! Please open up an issue if you would like to help out. :smile:
+
+## License
+Created with ♥ by [Tiaan du Plessis](https://github.com/mightyCrow). Licensed under the MIT License.
+
---
+++ index.js
@@ -0,0 +1,91 @@
+#! /usr/bin/env node
+
+const Timr = require('timrjs')
+const notifier = require('node-notifier')
+const vorpal = require('vorpal')()
+const pkg = require('./package.json')
+require('update-notifier')({pkg}).notify()
+
+// Default args if none provided
+const defaultTime = '25:00'
+const chillTime = '05:00'
+
+let timerRunning = false
+
+/**
+ * Clear current line and write text to it
+ */
+const clearLineAndWrite = (text) => {
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+  process.stdout.write(text)
+}
+
+/**
+ * Update terminal with current time
+ */
+const performTick = (time, timer, type) => {
+  timer.ticker((formattedTime) => {
+    if (!timerRunning) {
+      timer.stop()
+      clearLineAndWrite('🍅 ')
+    } else {
+      clearLineAndWrite(`🕐 ${formattedTime} - ${type}`)
+    }
+  })
+}
+
+/**
+ * Peform a Pomodoro session based on given args
+ */
+const peformPomodoro = (time, chill, cb) => {
+  timerRunning = true
+
+  // Setup session timer
+  const timer = Timr(time)
+  clearLineAndWrite(`🕐 ${time} - Session`)
+  timer.start()
+  performTick(time, timer, 'Session')
+
+  // Start break when session is done
+  timer.finish(() => {
+    notifier.notify({
+      title: pkg.name,
+      message: 'Pomodoro done!',
+      sound: true // Only Notification Center or Windows Toasters
+    })
+
+    // Setup chill timer
+    const timer = Timr(chill)
+    clearLineAndWrite(`🕐 ${chill} - chill`)
+    timer.start()
+    performTick(chill, timer, 'chill')
+
+    timer.finish(() => {
+      clearLineAndWrite('Done ✔︎\n')
+      notifier.notify({
+        title: pkg.name,
+        message: 'Chill done!',
+        sound: true
+      })
+
+      cb()
+    })
+  })
+}
+
+vorpal
+  .command('start', 'Start a Pomodoro')
+  .autocomplete(['--time', '--chill'])
+  .option('-t, --time <time>', 'Set the time of the Pomodoro. Default is 25:00 minutes.')
+  .option('-r, --chill <chill>', 'Set the time of chill. Default is 5:00 minutes.')
+  .action((args, cb) => {
+    peformPomodoro(args.options.time || defaultTime, args.options.chill || chillTime, cb)
+  })
+  .cancel(() => {
+    timerRunning = false
+  })
+
+vorpal
+  .delimiter('🍅')
+  .show()
